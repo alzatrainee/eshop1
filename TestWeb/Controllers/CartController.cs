07c453bbfce69dd4580e42bb16885e4a8a7deb4c
Cong Long Nguyen <nguyelon@cvut.cz>
2017-08-09 19:03:02 +0200

    Edited cartController

--- TestWeb/Controllers/CartController.cs
+++ TestWeb/Controllers/CartController.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Catalog.Business;
 
+
 namespace PernicekWeb.Controllers
 {
     public class CartController : Controller
     {
+        List<Item> cart = new List<Item>();
         public readonly CatalogService _catalogservice;
 
         public CartController(CatalogService catalogservice)
@@ -19,21 +21,32 @@ namespace PernicekWeb.Controllers
         {
             return View();
         }
-        // Add item to cart
-
-        public ActionResult AddToCart(int id)
+        //Check if an item exists in cart 
+        private int Contains(int id, List<Item> cart)
         {
-            if (ViewData["cart"] == null)
+            for (int i = 0; i < cart.Count; i++)
+                if (cart[i].P.id_pr == id)
+                    return i;
+            return -1;
+        }
+        // Add item to cart
+        public ActionResult Order(int id)
+        {
+            
+            if (cart == null)
             {
                 List<Item> cart = new List<Item>();
                 cart.Add(new Item(_catalogservice.GetProduct(id),1));
-                ViewData["cart"] = cart;
             }
             else
             {
-
+                int index = Contains(id, cart);
+                if (index == -1)
+                    cart.Add(new Item(_catalogservice.GetProduct(id), 1));
+                else
+                    cart[index].Quantity++;
             }
-            return View("Cart");
+            return View(cart);
 
         }
 
