27dffea34c149378e475ca791c4d86cb9bb4d8de
Cong Long Nguyen <nguyelon@cvut.cz>
2017-08-11 09:35:18 +0200

    Added blueberry cookies

--- TestWeb/Controllers/NudlesPlaygroundController.cs
+++ TestWeb/Controllers/NudlesPlaygroundController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Catalog.Business;
+using Catalog.Dal.Entities;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +10,17 @@ namespace PernicekWeb.Controllers
 {
     public class NudlesPlaygroundController : Controller
     {
-
+        private readonly CatalogService _catalogservice;
+        public NudlesPlaygroundController(CatalogService catalogservice)
+        {
+            _catalogservice = catalogservice;
+        }
+        public IActionResult Index()
+        {
+            List<Product> products = _catalogservice.GetAllProducts();
+            HttpContext.Items["products"] = products;
+            return View();
+        }
 
     }
 }
