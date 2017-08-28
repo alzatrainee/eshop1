c773a3cf3d565eb93c9f1e0c1c4c4f2dd122c92b
tonyldt <letuan@fit.cvut.cz>
2017-08-28 18:07:47 +0200

    Revert "Edited cart"
    This reverts commit 80c1b6be3b94bad31f4fad013019e505d5735862.

--- Business/Dal/Entity/Cart_pr.cs
+++ Business/Dal/Entity/Cart_pr.cs
@@ -15,26 +15,24 @@ namespace Module.Business.Dal.Entities
         
         [Required]
         public int amount { get; set; }
-        public string id_col { get; set; }
+
         public int id_si { get; set; }
+
+        public string id_col { get; set; }
 
         public Cart Cart { get; set; }
         public Product Product { get; set; }
 
-        public Cart_pr(int id_car, int Idecko, int amount, int Sizes, string Colours)
+        public Cart_pr(int id_car, int id_pr, int amount, int id_si, string id_col)
         {
             this.id_car = id_car;
-            this.id_pr = Idecko;
+            this.id_pr = id_pr;
             this.amount = amount;
-            this.id_si = Sizes;
-            this.id_col = Colours;
+            this.id_si = id_si;
+            this.id_col = id_col;
         }
 
-        public Cart_pr()
-        {
-            amount = 0;
-        }
+        public Cart_pr() { }
 
-        public List<Product> LiProduct { get; set; } = new List<Product>();
     }
 }
--- TestWeb/Controllers/OrderController.cs
+++ TestWeb/Controllers/OrderController.cs
@@ -11,8 +11,7 @@ using Module.Business.Dal.Entities;
 using Module.Business.Business;
 using Module.Business.Dal.Entity;
 using Pernicek.Models.PlaygroundViewModels;
-using PernicekWeb.Models.OrderViewModels;
-using Pernicek.Controllers;
+using PernicekWeb.Models.CatalogViewModel;
 
 namespace PernicekWeb.Controllers
 {
@@ -61,18 +60,17 @@ namespace PernicekWeb.Controllers
             ViewData["CartItem"] = cartItems;
             return View(cartItems);
         }
+        public ActionResult Order()
+        {
+            Product product = new Product();
+            return View(product);
+        }
 
         // Add item to cart
         //
         // GET: /Cart/Order
         [HttpGet]
-
         public async Task<ActionResult> Order(int Idecko, string Colours, int Sizes, int amount)
-        {
-            return View();
-        }
-
-        public async Task<ActionResult> Index(int Idecko, string Colours, int Sizes, int amount)
         {
             /*
             if (_signInManager.IsSignedIn(User))
@@ -87,7 +85,7 @@ namespace PernicekWeb.Controllers
             */
             var user = await _userManager.GetUserAsync(User);
 
-            
+
 
             var tmp = _businessservice.GetCart(user.Id);
 
@@ -104,7 +102,7 @@ namespace PernicekWeb.Controllers
 
             var item = (Cart_pr)tmp.data;
             /*
-            var product = _catalogservice.GetProduct(item.id_pr);  
+            var product = _catalogservice.GetProduct(item.id_pr);
             var image = _catalogservice.GetImage(item.id_pr);
             var firm = _catalogservice.GetFirm(product.id_fir);
             var viewModel = new OrderProduct
@@ -113,43 +111,15 @@ namespace PernicekWeb.Controllers
                 nameProduct = product.name,
                 Price = item.amount * product.price,
                 image = image.link,
-                Firm = firm.name/*,
+                Firm = firm.name,
                 colour = item.colour,
                 size = item.size
             };
-            viewModel.OrdProd.Add(viewModel);   */   
-          //  return RedirectToAction(nameof(PlaygroundController.Index), "Index", viewModel);
-            
+            model.OrdProd.Add(viewModel);      
+            return RedirectToAction(nameof(PlaygroundController.Index), "Index", viewModel);
+            */
             return View();
         }
-        [HttpPost]
-        public async Task<IActionResult> Refresh([FromBody]OrderProduct viewModel)
-        {
-
-            var user = await _userManager.GetUserAsync(User);
-            var result = _businessservice.GetProductsCart(user.Id);
-
-            foreach (var item in result)
-            {
-                var product = _catalogservice.GetProduct(item.id_pr);
-                var image = _catalogservice.GetImage(item.id_pr);
-               // var firm = _catalogservice.GetFirm(product.id_fir);
-                //viewModel.id_pr = item.id_pr;
-                viewModel.nameProduct = product.name;
-                viewModel.Price = item.amount * product.price;
-                viewModel.image = image.link;
-               //     Firm = firm.name
-               /*,
-                colour = item.colour,
-                size = item.size*/
-
-                viewModel.OrdProd.Add(viewModel);
-            }
-            return Json(viewModel);
-                }
-
-
-
 
         // Delete item from Cart
         //
@@ -179,49 +149,20 @@ namespace PernicekWeb.Controllers
             return View();
         }
 
-        [HttpGet]
-        public async Task<ActionResult> Order(OrderProduct model)
-        {
-            var user = await _userManager.GetUserAsync(User);
-            var result = _businessservice.GetProductsCart(user.Id);
-
-            foreach (var item in result)
-            {
-                var product = _catalogservice.GetProduct(item.id_pr);
-                var image = _catalogservice.GetImage(item.id_pr);
-                var firm = _catalogservice.GetFirm(product.id_fir);
-                //viewModel.id_pr = item.id_pr;
-                model.nameProduct = product.name;
-                model.Price = item.amount * product.price;
-                model.image = image.link;
-                model.Firm = firm.name;
-                //     Firm = firm.name
-                /*,
-                 colour = item.colour,
-                 size = item.size*/
-
-                model.OrdProd.Add(model);
-            }
-            return View(model);
-        }
-
-        [HttpPost]
-        public async Task<ActionResult> FinishOrder(int? Payment, int? ShippingOption, OrderProduct model)
+        public async Task<ActionResult> NewOrder(int? Payment, int? Shipping, PlaygroundViewModel model)
         {
             decimal sumPrice = 0;
-
-            var user = await _userManager.GetUserAsync(User);
 
             if ( Payment == null) // nesmi nastat, uzivatel si musi vybrat zpusob platby
             {
                 return View();
             }
 
-            if (ShippingOption == null)
+            if (Shipping == null)
             {
-                ShippingOption = 1; // nesmi nastat, uzivatel si musi vybrat dopravu
+                return View(); // nesmi nastat, uzivatel si musi vybrat dopravu
             }
-            
+
 
             /* Pridani adresy do databaze */
             var address = new Address(model.street, model.city, model.house_number, model.post_code);
@@ -229,34 +170,31 @@ namespace PernicekWeb.Controllers
 
 
             /* Vytvoreni NewOrder a prida do databaze bez id_pay */
-            var NewOrder = new NewOrder(user.Id, 1, address.id_ad, ShippingOption.Value); // 1 je status objednavky
+            var user = await _userManager.GetUserAsync(User);
+            var NewOrder = new NewOrder(user.Id, 1, address.id_ad, Shipping.Value); // 1 je status objednavky
             _orderService.AddNewOrder(NewOrder);
 
 
-            /* Ziskani produkty z Cart_pr podle usera a vlozeni do databaze Order_prod */
+            /* Ziskani produkty z Cart_pr podle usera a vlozeni do databze Order_prod */
             var orderProd = _businessservice.GetConnectCart(user.Id);
             foreach (var item in orderProd)
             {
                 var orPr = new Order_prod(NewOrder.id_ord, item.id_pr, item.amount);
-                var pri = _catalogservice.GetProduct(item.id_pr);
-                sumPrice += pri.price;
+                sumPrice += item.Product.price; // tady zalezi jestli je to i v tom Productu, pokud ne bude to chtit jinej pristup
                 _businessservice.AddOrder_prod(orPr);
-                /* Vymazani produktu z Cart_pr */
-                _businessservice.DeleteCart_pr(item);
             }
 
 
             /* Vypocitani celkove ceny plus pridani Payment do databaze */
-             var ship = _orderService.GetPriceShipping(ShippingOption.Value);
+            var ship = _orderService.GetPriceShipping(Shipping.Value);
             sumPrice += ship.price;
             var payment = new Payment(Payment.Value, 1, sumPrice); // 1 je payment status
             _orderService.AddPayment(payment);
 
 
             /* Pokus o pridani id_pay do NewOrder */
-             NewOrder.id_pay = payment.id_pay;
-             _orderService.UpdateNewOrder(NewOrder);
-            
+            NewOrder.id_pay = payment.id_pay;
+            _orderService.AddNewOrder(NewOrder); //timhle si nejsem jistej bude to chtit otestovat a pripradne upravit
 
             return View();
         }
