80c1b6be3b94bad31f4fad013019e505d5735862
Cong Long Nguyen <nguyelon@cvut.cz>
2017-08-28 14:30:41 +0200

    Edited cart

--- Business/Dal/Entity/Cart_pr.cs
+++ Business/Dal/Entity/Cart_pr.cs
@@ -15,24 +15,26 @@ namespace Module.Business.Dal.Entities
         
         [Required]
         public int amount { get; set; }
-
-        public int id_si { get; set; }
-
         public string id_col { get; set; }
+        public int id_si { get; set; }
 
         public Cart Cart { get; set; }
         public Product Product { get; set; }
 
-        public Cart_pr(int id_car, int id_pr, int amount, int id_si, string id_col)
+        public Cart_pr(int id_car, int Idecko, int amount, int Sizes, string Colours)
         {
             this.id_car = id_car;
-            this.id_pr = id_pr;
+            this.id_pr = Idecko;
             this.amount = amount;
-            this.id_si = id_si;
-            this.id_col = id_col;
+            this.id_si = Sizes;
+            this.id_col = Colours;
         }
 
-        public Cart_pr() { }
+        public Cart_pr()
+        {
+            amount = 0;
+        }
 
+        public List<Product> LiProduct { get; set; } = new List<Product>();
     }
 }
