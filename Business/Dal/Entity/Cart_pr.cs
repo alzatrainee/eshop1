c5179aefd00d18ce7e237c769f9d05de5519b849
tonyldt <letuan@fit.cvut.cz>
2017-08-30 20:35:23 +0200

    Frontend: Profile stránka, vyrenderování Košíku

--- Business/Dal/Entity/Cart_pr.cs
+++ Business/Dal/Entity/Cart_pr.cs
@@ -15,12 +15,27 @@ namespace Module.Business.Dal.Entities
         
         [Required]
         public int amount { get; set; }
-     //   public string id_col { get; set; }
-     //   public int id_si { get; set; }
 
-        public Cart Cart { get; set; }
+        public string id_col { get; set; }
+        public int id_si { get; set; }
+
+
+        //public Cart Cart { get; set; }
         public Product Product { get; set; }
+       // public Colour Colour { get; set; }
+        public Size Size { get; set; }
 
-        public List<Product> LiProduct { get; set; } = new List<Product>();
+        //public List<Product> LiProduct { get; set; } = new List<Product>();
+
+        public Cart_pr(int id_car, int id_pr, int amount, int id_si, string id_col)
+        {
+            this.id_pr = id_pr;
+            this.id_car = id_car;
+            this.amount = 1;
+            this.id_si = id_si;
+            this.id_col = id_col;
+        }
+        public Cart_pr() { }
+        
     }
 }
