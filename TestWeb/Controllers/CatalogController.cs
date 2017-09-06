eb4bb795ab2c59ede20be92f4cf17abfdab7019e
Marcel Novák <marcel.novak14@live.com>
2017-09-06 16:13:15 +0200

    edit price range

--- TestWeb/Controllers/CatalogController.cs
+++ TestWeb/Controllers/CatalogController.cs
@@ -32,7 +32,6 @@ namespace PernicekWeb.Controllers
             model.SortHigh = 1;
             model.SortLow = 1;
             model.NumbersLike = 1;
-            model.minPrice = 1;
 
             model.ItemsPerPage = itemsPerPage.Value; // Ukladam si polozek na stranku do modelu
             model.CurrentPage = page.Value; // pro zobrazeni soucasne stranky ve View
