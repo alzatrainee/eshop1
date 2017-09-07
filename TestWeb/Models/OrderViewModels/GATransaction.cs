f12201b864f88254b9782436f03dbd6bffb71dd5
Cong Long Nguyen <nguyelon@cvut.cz>
2017-09-07 23:50:25 +0200

    GA

---
+++ TestWeb/Models/OrderViewModels/GATransaction.cs
@@ -0,0 +1,24 @@
+﻿using Module.Business.Dal.Entities;
+using Module.Order.Dal.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PernicekWeb.Models.OrderViewModels
+{
+    public class GATransaction
+    {
+        public NewOrder Order { get; set; }
+        public List<Cart_pr> Items {get;set;}
+
+        public GATransaction() { }
+        public GATransaction(NewOrder Order, List<Cart_pr> Items)
+        {
+            this.Order = Order;
+            this.Items = Items;
+        }
+
+
+    }
+}
